3483562fcef10840f0f0979d994591a98233c350
Dexter <96929816+thedexter46@users.noreply.github.com>
2022-09-15 08:08:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/MC_Moviescafe')
+                InlineKeyboardButton('🍿 ᴜᴘᴅᴀᴛᴇ', url='https://t.me/MC_Moviescafe')
             ],
             [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
